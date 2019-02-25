a73453ddd97afca2e2e19a6926424fd7d393b1ea
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-02-25 16:13:34 +0100

    add header to book page

--- app/routes/book.js
+++ app/routes/book.js
@@ -2,7 +2,12 @@ import { inject } from '@ember/service';
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	session: inject(),
 	booksRepository: inject(),
+
+	beforeModel: function() {
+    return this.get('session').fetch().catch(function() {});
+  },
 
 	model: function (parameters) {
 		return this.get('booksRepository').getBookSync(parameters.book_id);

319aa07cbfd9c3ca190d31b1188a8daea7353a83
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-03-03 22:11:14 +0100

    WIP barcode scanner

--- app/components/barcode-reader.js
+++ app/components/barcode-reader.js
@@ -20,8 +20,8 @@ export default Component.extend({
 				},
 				decoder : {
 					readers: [
-						"ean_reader",
-						"ean_8_reader"
+					"ean_reader",
+					"ean_8_reader"
 					],
 				},
 				locate: true,
@@ -33,12 +33,31 @@ export default Component.extend({
 
 				Quagga.start();
 				$("#cameraView video").css("width", "100%");
-				$("#cameraView video").css("height", "100%");
+			});
+
+			Quagga.onProcessed(function (result) {
+				let drawingCtx = Quagga.canvas.ctx.overlay;
+				let drawingCanvas = Quagga.canvas.dom.overlay;
+
+				if (result) {
+					if (result.boxes) {
+						drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+						
+						result.boxes.filter((box) => {return box !== result.box;}).forEach((box) => {
+							Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "#fff", lineWidth: 2 });
+						});
+					}
+
+					if (result.box) {
+						Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#fff", lineWidth: 2 });
+					}
+
+					if (result.codeResult && result.codeResult.code) {
+						Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: '#ff6a6a', lineWidth: 3 });
+					}
+				}
 			});
 		}
-	},
-
-	closeBarcodeScanner: function () {
 
 	}
 

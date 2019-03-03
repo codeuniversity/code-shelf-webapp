166306cd826ed97d917e01c2f05090c1ed3e711d
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-03-03 13:44:59 +0100

    render availabilty status on details page

--- app/components/book-details.js
+++ app/components/book-details.js
@@ -1,4 +1,26 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+
+	availabilityClass: computed(function () {
+		let availabilty = this.get('book').status;
+
+		if (availabilty === "AVAILABLE") {
+			return 'available-book';
+		}
+		else {
+			return 'unavailable-book';
+		}
+	}),
+
+	formattedStatus: computed(function () {
+		return this.get('book')
+			.status
+			.toLowerCase()
+			.split(' ')
+			.map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+			.join(' ');
+	})
+
 });

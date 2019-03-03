75295daf151f9634eb5ec8fe5306125a9ac57ebb
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-03-03 17:35:30 +0100

    launch camera with quagga

--- app/components/barcode-reader.js
+++ app/components/barcode-reader.js
@@ -1,9 +1,26 @@
+import Quagga from 'npm:quagga';
 import Component from '@ember/component';
 
 export default Component.extend({
 
 	actions: {
 		launchBarcodeScanner: function () {
+			Quagga.init({
+				inputStream : {
+					name : "Live",
+					type : "LiveStream",
+					target: document.querySelector('#cameraView')
+				},
+				decoder : {
+					readers : ["code_128_reader"]
+				}
+			}, function(err) {
+				if (err) {
+					console.log(err);
+					return
+				}
+				Quagga.start();
+			});
 		}
 	}
 

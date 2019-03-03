a4b381281f0b2652d513c091d5d997f0ba7a423d
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-03-03 23:12:27 +0100

    remove barcode squares

--- app/components/barcode-reader.js
+++ app/components/barcode-reader.js
@@ -35,29 +35,6 @@ export default Component.extend({
 				$("#cameraView video").css("width", "100%");
 			});
 
-			Quagga.onProcessed(function (result) {
-				let drawingCtx = Quagga.canvas.ctx.overlay;
-				let drawingCanvas = Quagga.canvas.dom.overlay;
-
-				if (result) {
-					if (result.boxes) {
-						drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-						
-						result.boxes.filter((box) => {return box !== result.box;}).forEach((box) => {
-							Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "#fff", lineWidth: 2 });
-						});
-					}
-
-					if (result.box) {
-						Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#fff", lineWidth: 2 });
-					}
-
-					if (result.codeResult && result.codeResult.code) {
-						Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: '#ff6a6a', lineWidth: 3 });
-					}
-				}
-			});
-
 			Quagga.onDetected((result) => {
         alert(result.codeResult.code);
       });

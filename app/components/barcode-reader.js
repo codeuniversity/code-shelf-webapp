87128ea969afd9ba60fcc7b2638111215bff641c
Tobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2019-05-07 06:30:03 +0200

    Implemented camera toggle

--- app/components/barcode-reader.js
+++ app/components/barcode-reader.js
@@ -3,49 +3,58 @@ import Quagga from 'npm:quagga';
 import { inject } from '@ember/service';
 import Component from '@ember/component';
 
+let active = false;
+
 export default Component.extend({
 	booksRepository: inject(),
 
 	actions: {
 
 		launchBarcodeScanner: function () {
-			$("#cameraView").css("display", "block");
+			if (!active) {
+				$("#cameraView").css("display", "block");
 
-			Quagga.init({
-				inputStream : {
-					name : "Live",
-					type : "LiveStream",
-					target: document.querySelector('#cameraView'),
-					constraints: {
-						facingMode: "environment"
+				Quagga.init({
+					inputStream : {
+						name : "Live",
+						type : "LiveStream",
+						target: document.querySelector('#cameraView'),
+						constraints: {
+							facingMode: "environment"
+						}
+					},
+					decoder : {
+						readers: [
+							"ean_reader",
+							"ean_8_reader"
+						],
+					},
+					locate: true,
+				}, function(err) {
+					if (err) {
+						console.log(err);
+						return
 					}
-				},
-				decoder : {
-					readers: [
-						"ean_reader",
-						"ean_8_reader"
-					],
-				},
-				locate: true,
-			}, function(err) {
-				if (err) {
-					console.log(err);
-					return
-				}
 
-				Quagga.start();
-			});
+					Quagga.start();
+					active = true;
+				});
 
-			Quagga.onDetected((result) => {
-				let preview = this.get('booksRepository').getPreview(result.codeResult.code);
+				Quagga.onDetected((result) => {
+					let preview = this.get('booksRepository').getPreview(result.codeResult.code);
 
-				if (preview.status === 404) {
-					alert("No matching books found on google books.");
-					// TODO style notification
-				} else {
-					window.location.href = '/preview/' + result.codeResult.code;
-				}
-      });
+					if (preview.status === 404) {
+						alert("No matching books found on google books.");
+						// TODO style notification
+					} else {
+						window.location.href = '/preview/' + result.codeResult.code;
+					}
+	      });
+			}
+			else {
+				Quagga.stop();
+				active = false;
+			}
 		}
 
 	}
